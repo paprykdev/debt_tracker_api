25e8cca40a38df265fb2703a58bdc8439cf2ae6c
paprykdev <58005447+paprykdev@users.noreply.github.com>
2025-01-03 00:09:36 +0100

    feat(env): add dotenv file support
    Signed-off-by: paprykdev <58005447+paprykdev@users.noreply.github.com>

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,9 +1,17 @@
 use actix_web::{web, App, HttpServer};
 use debt_tracker_api::routes::route::{add_debt, get_all_debts, get_debt_by_id};
+use dotenv::dotenv;
+use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let uri = std::env::var("MONGODB_URI").unwrap_or_else(|_| format!("mongodb+srv://papryk:<passwd>@cluster0.yuwkm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"));
+    dotenv().ok();
+
+    let username = env::var("MONGO_USER").expect("MONGO_USER must be set");
+    let password = env::var("MONGO_PASSWD").expect("MONGO_PASSWD must be set");
+
+    let uri = std::env::var("MONGODB_URI")
+        .unwrap_or_else(|_| format!("mongodb+srv://{}:{}@cluster0.yuwkm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", username, password));
 
     let client = mongodb::Client::with_uri_str(&uri)
         .await
