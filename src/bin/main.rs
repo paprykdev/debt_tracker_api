4265557d0cd22a2ab1837db26cbc226b176c4a5e
paprykdev <58005447+paprykdev@users.noreply.github.com>
2025-01-03 10:45:18 +0100

    feat(env): add ip and port env support
    Signed-off-by: paprykdev <58005447+paprykdev@users.noreply.github.com>

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,5 +1,8 @@
 use actix_web::{web, App, HttpServer};
-use debt_tracker_api::routes::route::{add_debt, get_all_debts, get_debt_by_id};
+use debt_tracker_api::routes::route::{
+    add_debt, delete_debt, get_all_debts, get_debt_by_id, update_debt,
+};
+
 use dotenv::dotenv;
 use std::env;
 
@@ -7,8 +10,14 @@ use std::env;
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
 
-    let username = env::var("MONGO_USER").expect("MONGO_USER must be set");
-    let password = env::var("MONGO_PASSWD").expect("MONGO_PASSWD must be set");
+    let username = env::var("MONGO_USER").expect("MONGO_USER or MONGODB_URI must be set");
+    let password = env::var("MONGO_PASSWD").expect("MONGO_PASSWD or MONGODB_URI must be set");
+    let ip = env::var("IP_ADDRESS").expect("IP_ADDRESS must be set");
+    let port: u16 = env::var("PORT")
+        .unwrap_or("8080".into())
+        .trim()
+        .parse()
+        .expect("PORT must be a number");
 
     let uri = std::env::var("MONGODB_URI")
         .unwrap_or_else(|_| format!("mongodb+srv://{}:{}@cluster0.yuwkm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", username, password));
@@ -23,8 +32,10 @@ async fn main() -> std::io::Result<()> {
             .service(get_all_debts)
             .service(get_debt_by_id)
             .service(add_debt)
+            .service(delete_debt)
+            .service(update_debt)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((ip, port))?
     .run()
     .await
 }
